f811d444f96e1942f90dae111c18bf74d32518c2
unknown <mailbokstoo@hotmail.fr>
2024-01-23 14:38:07 +0100

    dernier push avant twig

--- src/Controller/PostsAdminController.php
+++ src/Controller/PostsAdminController.php
@@ -11,48 +11,46 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/posts')]
-class PostsController extends AbstractController
+#[Route('/admin/posts')]
+class PostsAdminController extends AbstractController
 {
-    #[Route('/', name: 'app_posts_index', methods: ['GET'])]
+    #[Route('/', name: 'app_admin_posts_index', methods: ['GET'])]
     public function index(PostsRepository $postsRepository): Response
     {
-        return $this->render('posts/index.html.twig', [
-            'posts' => $postsRepository->findBy(['fk_user' => ['IS NOT NULL']]),
+        return $this->render('postsAdmin/index.html.twig', [
+            'posts' => $postsRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_posts_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_posts_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $post = new Posts();
         $form = $this->createForm(PostsType::class, $post);
         $form->handleRequest($request);
 
-
         if ($form->isSubmitted() && $form->isValid()) {
-
             $entityManager->persist($post);
             $entityManager->flush();
 
             return $this->redirectToRoute('app_posts_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('posts/new.html.twig', [
+        return $this->render('postsAdmin/new.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_posts_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_admin_posts_show', methods: ['GET'])]
     public function show(Posts $post): Response
     {
-        return $this->render('posts/show.html.twig', [
+        return $this->render('postsAdmin/show.html.twig', [
             'post' => $post,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_posts_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_posts_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Posts $post, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(PostsType::class, $post);
@@ -64,13 +62,13 @@ class PostsController extends AbstractController
             return $this->redirectToRoute('app_posts_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('posts/edit.html.twig', [
+        return $this->render('postsAdmin/edit.html.twig', [
             'post' => $post,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_posts_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_admin_posts_delete', methods: ['POST'])]
     public function delete(Request $request, Posts $post, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
